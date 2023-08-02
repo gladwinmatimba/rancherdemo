89afc0d9470cb5641fc6896a46ee4c15c5ff45f3
F5210933 <gladwin.mbhalati@fnb.co.za>
2023-08-02 19:38:26 +0200

    initial commit

---
+++ src/main/java/com/example/rancherdemo/RancherdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rancherdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RancherdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RancherdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rancherdemo/RancherdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rancherdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RancherdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
